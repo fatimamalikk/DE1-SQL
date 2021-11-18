0caafe817e60938533b20bd00c7ceb92ec4cc259
Fatima Arshad <45011596+fatimamalikk@users.noreply.github.com>
2021-11-19 03:11:26 +0500

    Airbnb Austin Texas Dataset
    This folder contains all the work files used for the Data Engineering term project 1. I set out to build a MySQL schema, datawarehouses, datamarts, stored procedures, events, and triggers using the Airbnb Austin, Texas dataset available on the Inside Airbnb website.

---
+++ Term_Project_1/DE1_fatima_arshad.sql
@@ -0,0 +1,320 @@
+-- ---- OPERATIONAL LAYER -----
+
+SHOW VARIABLES LIKE "secure_file_priv";
+SHOW VARIABLES LIKE "local_infile";
+
+-- Create database schema 'airbnb' to store data for Toronto Airbnbs
+DROP SCHEMA IF EXISTS airbnb;
+CREATE SCHEMA airbnb;
+
+-- Set airbnb schema as default
+USE airbnb;
+
+-- Create table 'hosts' in airbnb schema
+DROP TABLE IF EXISTS hosts;
+CREATE TABLE hosts (
+    host_id INT NOT NULL,
+    PRIMARY KEY(host_id),
+    host_name VARCHAR(100),
+    host_since DATE,
+    host_is_superhost CHAR(5),
+    host_listings_count INT
+);
+
+-- Import host.csv data into hosts table
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\hosts_austin.csv' 
+INTO TABLE hosts 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 LINES  
+(host_id, host_name, @host_since, host_is_superhost, @host_listings_count) 
+SET 
+	host_since = nullif(@host_since, ''), 
+	host_listings_count = nullif(@host_listings_count,'');
+-- select * from airbnb.hosts;
+
+-- Create table 'listings' in airbnb schema
+DROP TABLE IF EXISTS listings;
+CREATE TABLE listings (
+    listing_id INT NOT NULL,
+    PRIMARY KEY (listing_id),
+    host_id INT NOT NULL,
+    listing_name VARCHAR(255),
+    listing_description VARCHAR(10500),
+    property_type VARCHAR(50),
+    accommodates VARCHAR(50),
+    bathrooms VARCHAR(25),
+    bedrooms INT,
+    beds INT,
+    price INT,
+    minimum_nights INT,
+    maximum_nights INT,
+    number_of_reviews INT,
+    review_scores_rating INT,
+    FOREIGN KEY(host_id) REFERENCES airbnb.hosts(host_id));
+    
+    
+-- Import listing_austin.csv data into listings table
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\listings_austin.csv' 
+INTO TABLE listings 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 LINES 
+(listing_id,host_id,listing_name,listing_description,property_type,accommodates,bathrooms,@bedrooms,@beds,@price,@minimum_nights,@maximum_nights,number_of_reviews,@review_scores_rating) 
+SET 
+	bedrooms = nullif(@bedrooms, ''), 
+	beds = nullif(@beds, ''), 
+	price = nullif(@price, ''), 
+	minimum_nights = nullif(@minimum_nights, ''), 
+	maximum_nights = nullif(@maximum_nights, ''), 
+	review_scores_rating = nullif(@review_scores_rating , '');
+
+-- LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/listings_austin.csv' INTO TABLE listings FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (listing_id,host_id,listing_name,listing_description,property_type,accommodates,bathrooms,@bedrooms,@beds,@price,@minimum_nights,@maximum_nights,number_of_reviews,@review_scores_rating) SET bedrooms = nullif(@bedrooms, ''), beds = nullif(@beds, ''), price = nullif(@price, ''), minimum_nights = nullif(@minimum_nights, ''), maximum_nights = nullif(@maximum_nights, ''), review_scores_rating = nullif(@review_scores_rating , '');
+-- select * from airbnb.listings;
+
+-- Create table 'calendar' in airbnb schema
+DROP TABLE IF EXISTS calendar;
+CREATE TABLE calendar (
+    listing_id INT NOT NULL,
+    available_date DATE,
+    available VARCHAR(5),
+    price INT,
+    minimum_nights INT,
+    maximum_nights INT,
+    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
+);
+
+-- Import calendar_austin.csv data into calendar table
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\calendar_austin.csv' 
+INTO TABLE calendar 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 LINES  
+(listing_id, available_date, available, @price, @minimum_nights, @maximum_nights) 
+SET 
+	price = nullif(@price, ''), 
+	minimum_nights = nullif(@minimum_nights, ''), 
+	maximum_nights = nullif(@maximum_nights, '');
+
+-- LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/calendar_austin.csv' INTO TABLE calendar FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES  (listing_id, available_date, available, @price, @minimum_nights, @maximum_nights) SET price = nullif(@price, ''), minimum_nights = nullif(@minimum_nights, ''), maximum_nights = nullif(@maximum_nights, '');
+-- select * from airbnb.calendar;
+
+-- #############################################Stored Procedures#####################################################
+
+-- Create procedure for 'Available listings' data warehouse
+DROP PROCEDURE IF EXISTS Get_available_listings;
+
+DELIMITER $$
+
+CREATE PROCEDURE Get_available_listings()
+BEGIN
+	DROP TABLE IF EXISTS available_listings;
+	CREATE TABLE available_listings AS
+	SELECT 
+	   listings.listing_id,
+	   listings.listing_name,
+	   calendar.available_date,
+       listings.property_type,
+       listings.accommodates,
+       listings.beds,
+       calendar.minimum_nights,
+       calendar.maximum_nights,
+       calendar.price,
+       ROUND(calendar.price/listings.accommodates)  AS price_per_person,
+	   hosts.host_id,
+       hosts.host_name
+	FROM
+		listings
+	INNER JOIN
+		calendar USING (listing_id)
+	INNER JOIN
+		hosts USING (host_id)
+	WHERE available = 't'
+	ORDER BY available_date;
+    
+    ALTER TABLE available_listings
+    MODIFY price_per_person FLOAT
+;
+END $$
+DELIMITER ;
+
+Call Get_available_listings();
+
+-- View Data Warehouse
+SELECT * FROM available_listings;
+
+-- Create  for 'host_ratings' data warehouse
+
+DROP PROCEDURE IF EXISTS Get_host_ratings;
+
+DELIMITER $$
+
+CREATE PROCEDURE Get_host_ratings()
+BEGIN
+
+DROP TABLE IF EXISTS host_ratings;
+CREATE TABLE host_ratings
+SELECT host_id,
+	host_name,
+	host_since,
+	host_is_superhost,
+	number_of_reviews,
+	host_listings_count,
+	ROUND(avg(review_scores_rating),1) AS host_rating
+	FROM listings
+	INNER JOIN hosts
+	USING(host_id)
+	GROUP BY host_id;
+    
+    ALTER TABLE host_ratings
+    MODIFY host_rating FLOAT;
+    
+END $$
+
+DELIMITER ;
+
+Call Get_host_ratings();
+
+-- View Data Warehouse
+SELECT * FROM host_ratings;
+
+-- ###################################################TRIGGERS############################################
+
+DROP TABLE IF EXISTS listings_audit;    
+
+CREATE TABLE listings_audit (
+	listing_id INT NOT NULL PRIMARY KEY,
+    host_id INT NOT NULL,
+    listing_name VARCHAR(255),
+    listing_description VARCHAR(10500),
+    property_type VARCHAR(50),
+    accommodates VARCHAR(50),
+    bathrooms VARCHAR(25),
+    bedrooms INT,
+    beds INT,
+    price INT,
+    minimum_nights INT,
+    maximum_nights INT,
+    number_of_reviews INT,
+    review_scores_rating INT,
+    updatedAt TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(listing_id) REFERENCES airbnb.listings(listing_id));
+
+DROP TRIGGER IF EXISTS before_host_info_update;
+
+DELIMITER $$
+
+CREATE TRIGGER before_host_info_update
+AFTER UPDATE        
+ON listings FOR EACH ROW
+BEGIN
+    INSERT INTO listings_audit(listing_id,host_id,listing_name,listing_description,property_type,accommodates,bathrooms,bedrooms,beds,price,minimum_nights,maximum_nights,number_of_reviews,review_scores_rating)
+    VALUES(OLD.listing_id,OLD.host_id,OLD.listing_name,OLD.listing_description,OLD.property_type,OLD.accommodates,OLD.bathrooms,OLD.bedrooms,OLD.beds,OLD.price,OLD.minimum_nights,OLD.maximum_nights,OLD.number_of_reviews,OLD.review_scores_rating);
+END$$
+
+DELIMITER ;
+
+-- TEST TRIGGER BY UPDATING ROW IN LISTING TABLES
+
+UPDATE listings
+SET 
+	number_of_reviews = 28,
+    review_scores_rating = 78
+WHERE
+    listing_id = 2265;
+ 
+SELECT * FROM listings_audit;
+
+-- ################################################VIEWS##########################################
+
+-- Create VIEW for summary statistics of property type in relation to price
+DROP VIEW IF EXISTS property_type_stats; 
+
+CREATE VIEW `property_type_stats` AS
+SELECT property_type AS 'Property Type',
+		COUNT(property_type) AS 'No of Listings',
+	   ROUND(Min(price)) AS 'Min Price (Total)',
+       ROUND(Max(price)) AS 'Max Price (Total)',
+       ROUND(AVG(price)) AS 'Avg Price (Total)',
+       ROUND( MIN(price/accommodates)) AS 'Min Price (Person)',
+       ROUND(MAX(price/accommodates)) AS 'Max Price (Person)',
+       ROUND(AVG(price/accommodates)) AS 'Avg Price (Person)'
+FROM available_listings
+GROUP BY property_type
+ORDER BY property_type;
+
+SELECT * FROM property_type_stats;
+
+
+-- Hosts with host rating score less than 50 | Materialized View with Event
+
+CREATE TABLE messages (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    message VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+SET GLOBAL event_scheduler = ON;
+
+-- Create a VIEW through an EVENT
+DELIMITER $$
+
+CREATE EVENT host_rating_warning_refresh
+ON SCHEDULE EVERY 1 MINUTE
+STARTS CURRENT_TIMESTAMP
+ENDS CURRENT_TIMESTAMP + INTERVAL 5 minute -- 5 minutes set for test purposes
+ON COMPLETION PRESERVE
+DO
+	BEGIN
+		DROP VIEW IF EXISTS host_rating_warning;
+        
+		CREATE VIEW host_rating_warning AS
+		SELECT *
+		FROM host_ratings
+		WHERE host_rating < 65 AND number_of_reviews > 2 -- criteria to filter put bad performers
+		ORDER BY host_rating DESC;
+
+		INSERT INTO messages(message,created_at)
+		VALUES('Event was generated. Host_rating_warning view was updated.',NOW());
+	
+	END$$
+DELIMITER ;
+
+SELECT * FROM host_rating_warning;
+SELECT * FROM messages;
+
+-- Create view for top 5 hosts through event which will run every 30 days for 5 months
+DROP EVENT IF EXISTS top_5_hosts_monthly;
+
+DELIMITER $$
+
+CREATE EVENT top_5_hosts_monthly
+ON SCHEDULE EVERY 30 DAY
+STARTS CURRENT_TIMESTAMP
+ENDS CURRENT_TIMESTAMP + INTERVAL 5 MONTH-- 5 minutes set for test purposes
+ON COMPLETION PRESERVE
+DO
+BEGIN
+DROP VIEW IF EXISTS top_5_superhosts;
+
+CREATE VIEW `top_5_superhosts` AS
+SELECT 
+host_id AS 'Host ID',
+host_name AS 'Host Name',
+host_since AS 'Host Since',
+number_of_reviews AS 'Number of Reviews',
+host_listings_count AS 'No of listings by Host',
+host_rating AS 'Host Rating'
+FROM host_ratings
+WHERE host_is_superhost = 't'
+ORDER BY
+host_rating DESC, number_of_reviews DESC, host_listings_count DESC, host_since DESC
+LIMIT 5;
+
+END$$
+DELIMITER ;
+
+-- Call the view
+Select * FROM top_5_superhosts;
---
+++ Term_Project_1/README.md
@@ -0,0 +1,114 @@
+## Airbnb Austin, Texas Dataset
+This folder contains all the work files used for the [ Data Engineering term project 1](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1). I set out to build a MySQL schema using the **Airbnb Austin, Texas dataset** available on the Inside Airbnb website - [link]( http://insideairbnb.com/get-the-data.html).
+
+### Task Interpretation ###
+Airbnb plans to conduct beta testing for a new business plan. They aim at increasing their revenue by incentivizing the hosts to maintain a positive score while reducing the costs. The top five hosts in a city will be rewarded according to their review scores. Airbnb is hoping that this will persuade the host to exhibit more hospitality, improve services, and customer relationships.
+
+Airbnb hopes that satisfied customers will leave a positive review that will attract the masses towards the hosts ultimately translating into revenue. The main competitors are the mainstream hotels. The beta test is planned to be conducted in Austin, a city in Texas, United States.
+
+Airbnb has a huge bulk of data to support their analytics team, but they are missing a central database to store this data. They gave me the task to design a system and provide high quality, easily interpretable data to the following three analytical groups:
+
+- The first one is the price unit which looks at the price statistics of different property types e.g. min, max, avg.
+- The second one is the host unit which looks at hosts with host rating scores less than 65 and the number of reviews greater than equal to 3. It will issue warning triggers so they can improve their services.
+- The third one is the super-host unit which identifies the top 10 super hosts every month, who will gain reward points.
+
+I decided to implement the solution in MYSQL RDBMS engine.
+
+### Airbnb Relational Database Schema ###
+The Airbnb schema comprises property listing data from [listings_austin.csv](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/data/listings_austin.csv) file, linked to the hosts’ table from [hosts_austin.csv](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/data/hosts_austin.csv) file on host_id, and calendar data from [calender_austin.csv](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/data/calender_austin.csv) file on listing_id.
+### OPERATIONAL LAYER ###
+My operational layer consists of [3 tables stored in csv](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/data) . The below EER diagram represents this schema. The **listings** table includes the properties listed by all hosts and the respective property attributes like property_type, bedrooms, bathrooms, price, reviews, etc. The **listings** table is linked to the **hosts** table with **host_id**. The **hosts** table includes the host particulars like name, the date on joining Airbnb, number of property listings, etc. Lastly, the **calendar** table shows the price and availability of the property on a particular date, which is linked to the **listings** table through **listing_id**. 
+
+![Schema](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/schema.png)
+
+The operational layer was created using the following [queries - Line 1 to 103](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/DE1_fatima_arshad.sql)
+
+### Analytics Plan
+
+My analytics plan is the following:
+1. Loading up the acquired data
+2. Create an ETL pipeline to create two data warehouses 
+3. Create an ETL pipeline to create data marts for the following analytics.
+
+This is illustrated in the below figure: 
+
+![Analytics plan diagram](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/analytics_plan.png)
+
+I created two data warehouses through stored procedures:
+1. **Available Listings Data Warehouse** focusing on property listings available by the host 
+2. **Hosts Rating Data Warehouse** focusing on host reviews. 
+
+I have also put a trigger in place which saves current information and stores it in a new table called **'listings_audit'** before any update is made.
+I have considered the following questions for analysis while creating the data warehouses:
+
+~~~~
+1. What are the price statistics 'overall' and 'per person' by property type in Austin, Texas, United States?
+~~~~
+I sliced this subset of the data from the **available_listings** data warehouse to compute the rounded MIN, MAX, and AVG prices grouped by property_type, for both overall and in_person scenarios. I sorted the table by property types in ascending alphabetical order.
+~~~~
+2. Which hosts have an overall rating of less than 65 with the number of reviews greater or equal to 3 to single out badly performing hosts?
+~~~~
+I used the **host_ratings** data warehouse to extract information and created a view through a scheduled event. This event creates a monthly listing i.e. every 30 days for concerning5 months to identify the hosts who need to be sent a warning. Moreover, a trigger saves a trigger-issued statement into a separate table **messages** to help us make sure that the trigger was executed.
+
+~~~~
+3. Who are the top 5 best performing hosts in terms of ratings and number of reviews?
+~~~~
+I used the **host_ratings** data warehouse to extract this information and created a view through a scheduled event. This event creates a monthly listing i.e. every 30 days for 5 months to identify hosts eligible for the reward points.
+
+
+
+### ANALYTICAL LAYER and ETL PIPELINE ###
+I created a denormalized snapshot of combined listings and hosts tables for the available_listings subject. I created in a stored procedure that contains commands to extract, transform and load the data into a new table. This combination of important variables from different tables into a single table i.e. a data warehouse will help us in further analysis for our new business plan. An analytical layer was created using the following queries:
+
+**Available Listings**
+![availability_listings](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/available_listings.png)
+
+**Host Ratings** 
+Next, I created a data warehouse through a stored procedure that encloses host-related information such as the number of reviews and ratings.
+
+![host_ratings](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/host_ratings.PNG)
+
+#### TRIGGERS ####
+Furthermore, I have put a trigger in place to save the current information for a listing, before the user updates it. This helps to keep track of all the host and listings activity in case of any technical or legal issue.
+A trigger is a stored program that is invoked automatically in response to an ‘action’ such as an insert, update or delete that occurs in the associated table which is useful for tracking changes to the data in the database. 
+For this, I created a new **listings_audit** table to save the old information before its updated. 
+
+The Before Host Info Update Trigger was created using the following [Queries - LINE 183 to 218](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/DE1_fatima_arshad.sql)
+Once the trigger had been created, we tested the trigger to ensure that it works using the following [test code - LINE 219 to 229]()
+~~~~
+UPDATE listings
+SET 
+	number_of_reviews = 29,
+	review_scores_rating = 78
+WHERE
+    listing_id = 2265;
+~~~~
+
+
+The trigger runs successfully and the listings_audit table is updated with the old information.
+
+![warning_trigger](https://github.com/fatimamalikk/DE1-SQL/tree/main/Term_Project_1/trigger_warning.PNG)
+
+
+### DATAMARTS with VIEWS ###
+I created the following three data marts for BI operations and analytics:
+1. Price View: 
+This view shows the price statistics with respect to the property type. It includes the minimum, maximum, and average price ‘overall’ and ‘per person.
+
+2. Low performing hosts: 
+This view enlists hosts to whom a warning should be issued due to their host rating scores being less than 65 and the number of reviews being greater than equal to 3. Such metrics will indicate that the hosts need to improve the experience that they provide to the users. This warning will incentivize the hosts to earn more reward points by working on the loopholes that exist and improving their rating.
+
+3. Top performing hosts: 
+This view enlists the top 5 hosts with the highest rating and customer satisfaction.
+
+
+##### Low Perfroming Hosts
+
+This view highlights the hosts that are consistently exhibiting poor performance. Hosts having reviews below a certain threshold will be warned by the system. The warning will intimate them about their unsatisfied clientage and their risk of losing reward points. It is expected that such a measure will persuade the host to provide better and more professional service leading to more revenue.
+
+##### Top Five Hosts #####
+
+The goal of this view is to display the top five hosts from the data. Depending upon the specified metrics, the top five hosts will be displayed. They shall be awarded reward points. The purpose of the points is to incentivize the hosts to provide better service to the users. Users will be attracted to hosts with higher points generating more revenue. This creates a win-win situation for the entire system.
+
+##### Price View #####
+This view will display the hosts providing the best service at the lowest costs. This view will help users on a budget who are looking for good service. Users looking for affordable accommodation without having to compromise on hospitality will be the prime targets. The overall objective is to make the service as inclusive as possible for all the strata of the societies.
